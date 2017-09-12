6f787ff1bfbbede8c82a467f114403007d7db9f3
BirdZhang <0312birdzhang@gmail.com>
2017-09-12 18:14:20 +0800

    [wip]update

--- qml/py/main.py
+++ qml/py/main.py
@@ -10,6 +10,27 @@ client.configure(**{
 })
 
 
+def login(user, password):
+    status_code, userInfo = client.users.login(user, password)
+    if not status_code:
+        return False
+    return userInfo
+
+
+def post(title, content, uid, cid):
+    status_code, response = client.topics.create(uid, cid, title, content)
+    if not status_code or status_code != 200:
+        return False
+    return True
+
+def getrecent():
+    status_code, topics = client.topics.get_recent()
+    
+
+def listcategory():
+    status_code, categories = client.categories.list()
+    for category in categories:
+        print(category["cid"], category["name"])
 
 #pyotherside.send('loadStarted')
 #pyotherside.send('loadFinished')
