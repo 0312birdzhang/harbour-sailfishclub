b7757b33dd4e236579c899156541dd9b66027b5d
BirdZhang <0312birdzhang@gmail.com>
2019-03-20 21:40:11 +0800

    Fix cacert issue

--- py/certifi/core.py
+++ py/certifi/core.py
@@ -8,11 +8,12 @@ This module returns the installation location of cacert.pem.
 """
 import os
 
-
 def where():
-    f = os.path.dirname(__file__)
+#    f = os.path.dirname(__file__)
+#   hard code
+    f = "/usr/share/"
 
-    return os.path.join(f, 'cacert.pem')
+    return os.path.join(f,'harbour-sailfishclub','qml', 'cacert.pem')
 
 
 if __name__ == '__main__':
